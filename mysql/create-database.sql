a33d1675483b36c6691445988d19b1e9c5c0c4b2
Kazuki Okamoto <kazuki.okamoto@kakkun61.com>
2016-01-08 20:34:28 +0900

    init MySQL

---
+++ mysql/create-database.sql
@@ -0,0 +1,4 @@
+# Run following.
+# mysql -u root < create-database.sql
+
+CREATE SCHEMA IF NOT EXISTS `twitter-review` DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_bin;
---
+++ mysql/create-user.sql
@@ -0,0 +1,5 @@
+# Run following.
+# mysql -u root -e "SET @password = '${MYSQL_PASSWORD}'; SOURCE create-user.sql;"
+
+SET @create_user = CONCAT("CREATE USER `twitter-review`@`localhost` IDENTIFIED BY '", @password, "'");
+PREPARE stmt FROM @create_user; EXECUTE stmt; DEALLOCATE PREPARE stmt;
