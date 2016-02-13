1cb16c9c1887a218e0253b7b9609beadc191c9ca
Kazuki Okamoto <kazuki.okamoto@kakkun61.com>
2016-02-14 04:04:55 +0900

    give permissions

--- mysql/create-user.sql
+++ mysql/create-user.sql
@@ -3,3 +3,4 @@
 
 SET @create_user = CONCAT("CREATE USER `twitter-review`@`localhost` IDENTIFIED BY '", @password, "'");
 PREPARE stmt FROM @create_user; EXECUTE stmt; DEALLOCATE PREPARE stmt;
+grant all on `twitter-review`.* to 'twitter-review'@'localhost';
