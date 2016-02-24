c96db004c933c0ace09ffb1d244a51739f450f49
Kazuki Okamoto <kazuki.okamoto@kakkun61.com>
2016-02-25 00:36:10 +0900

    fromSqlKey

--- app/Handler/Tweet.hs
+++ app/Handler/Tweet.hs
@@ -2,6 +2,7 @@ module Handler.Tweet where
 
 import Import
 import qualified Data.Set as S
+import Database.Persist.Sql
 
 getTweetR :: ScreenName -> TweetId -> Handler Html
 getTweetR screenName statusId = do-- error "Not yet implemented: getTweetR"
@@ -12,5 +13,5 @@ postTweetR screenName statusId = do
     johnId <- runDB $ insert $ User "john" Nothing
 --     let tweet = Tweet "Hi" johnId (S.fromList [])
     defaultLayout [whamlet|
-                      <p>user id: #{johnId}
+                      <p>user id: #{fromSqlKey johnId}
                   |]
