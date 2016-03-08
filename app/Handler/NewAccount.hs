4a6856b1cace2a7204bfd5a0dd5f577d51c48f8a
Kazuki Okamoto <kazuki.okamoto@kakkun61.com>
2016-03-08 09:08:18 +0900

    add Twitter account handler

---
+++ app/Handler/NewAccount.hs
@@ -0,0 +1,9 @@
+module Handler.NewAccount where
+
+import Import
+
+getNewAccountR :: Handler Html
+getNewAccountR = error "not yet implemented"
+
+postNewAccountR :: Handler Html
+postNewAccountR = error "not yet implemented"
