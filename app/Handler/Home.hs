97a18046977ba55114ef16ede22c2a0c4f6c7487
Kazuki Okamoto <kazuki.okamoto@kakkun61.com>
2016-03-21 18:48:58 +0900

    account list

--- app/Handler/Home.hs
+++ app/Handler/Home.hs
@@ -1,12 +1,27 @@
 module Handler.Home where
 
 import Import
-import Database.Persist.Sql
 
 getHomeR :: Handler Html
 getHomeR = do
-    mUser <- fmap (fmap entityVal) maybeAuth
-    defaultLayout $ do
-        let signinWithGoogle = $(widgetFile "signin-with-google")
-        headerWidget mUser
-        $(widgetFile "home")
+    mUser <- maybeAuth
+    case mUser of
+        Just user ->
+            runDB $ do
+                reps <- selectList [UserAccountRelationUserId ==. entityKey user] []
+                $(logDebug) $ pack $ show reps
+                accounts <- filterJust <$> mapM (get . userAccountRelationAccountId . entityVal) reps
+                lift $ defaultLayout $ do
+                    let signinWithGoogle = $(widgetFile "signin-with-google")
+                    headerWidget $ Just $ entityVal user
+                    $(widgetFile "home")
+                where
+                    filterJust (Just a : r)  = a : filterJust r
+                    filterJust (Nothing : r) = filterJust r
+                    filterJust []            = []
+        Nothing ->
+            defaultLayout $ do
+                let signinWithGoogle = $(widgetFile "signin-with-google")
+                let accounts = []
+                headerWidget $ entityVal <$> mUser
+                $(widgetFile "home")
