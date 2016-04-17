93694e050028f08797b1238543a42d85446d6d65
Kazuki Okamoto <kazuki.okamoto@kakkun61.com>
2016-04-17 18:35:31 +0900

    get account specific the newest tweet candidate
    debugged

--- app/Handler/Account.hs
+++ app/Handler/Account.hs
@@ -9,17 +9,19 @@ getAccountR accountIdParam = do
     runDB $ do
         accountEntity <- getBy404 (UniqueAccount accountIdParam)
         let account = entityVal accountEntity
-        tweetEntities <- selectList [TweetAccountId ==. entityKey accountEntity] [Desc TweetCreated]
-        rows <- E.select $ E.from $ \(tw `E.InnerJoin` twc) -> do
-            E.on $ tw E.^. TweetId E.==. twc E.^. TweetCandidateTweetId
+        rows <- E.select $ E.from $ \(tw `E.InnerJoin` twc `E.InnerJoin` acc `E.InnerJoin` usr) -> do
+            E.on $
+                tw E.^. TweetId E.==. twc E.^. TweetCandidateTweetId
+                E.&&. tw E.^. TweetAccountId E.==. acc E.^. AccountId
+                E.&&. twc E.^. TweetCandidateUserId E.==. usr E.^. UserId
             E.where_ $
-                tw E.^. TweetAccountId E.==. E.val (toSqlKey accountIdParam)
+                acc E.^. AccountIdent E.==. E.val accountIdParam
                 E.&&. E.notExists (E.from $ \twca ->
                                        E.where_ $
                                            twca E.^. TweetCandidateTweetId E.==. tw E.^. TweetId
                                            E.&&. twca E.^. TweetCandidateCreated E.>. twc E.^. TweetCandidateCreated
                                   )
-            return (tw, twc)
+            return (tw, twc, usr)
         $(logDebug) $ "rows: " ++ pack (show rows)
         lift $ defaultLayout $ do
             headerWidget $ Just user
