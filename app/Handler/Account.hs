160c6916e1dc67bf31d9fc766c082ad4dce16aa5
Kazuki Okamoto <kazuki.okamoto@kakkun61.com>
2016-04-21 01:29:10 +0900

    default open param

--- app/Handler/Account.hs
+++ app/Handler/Account.hs
@@ -2,34 +2,36 @@ module Handler.Account (getAccountR) where
 
 import Import
 import qualified Database.Esqueleto as E
-import Data.Text (splitOn)
-import Data.Set (Set)
 import qualified Data.Set as S
 
 getAccountR :: AccountIdParam -> Handler Html
 getAccountR accountIdParam = do
     user <- entityVal <$> requireAuth
-    statusParams <- fromMaybe S.empty . join . (fromParam <$>) <$> lookupGetParam "status"
-    $(logDebug) $ "state params: " <> pack (show (statusParams :: Set StatusParam))
-    (account, rows) <- runDB $ do
-        accountEntity <- getBy404 (UniqueAccount accountIdParam)
-        let account = entityVal accountEntity
-        rows <- E.select $ E.from $ \(tw `E.InnerJoin` twc `E.InnerJoin` acc) -> do
-            E.on $
-                tw E.^. TweetId E.==. twc E.^. TweetCandidateTweetId
-                E.&&. tw E.^. TweetAccountId E.==. acc E.^. AccountId
-            E.where_ $
-                acc E.^. AccountIdent E.==. E.val accountIdParam
-                E.&&. E.notExists (E.from $ \twca ->
-                                       E.where_ $
-                                           twca E.^. TweetCandidateTweetId E.==. tw E.^. TweetId
-                                           E.&&. twca E.^. TweetCandidateCreated E.>. twc E.^. TweetCandidateCreated
-                                  )
-            return (tw, twc)
-        return (account, rows)
-    defaultLayout $ do
-        headerWidget $ Just user
-        $(widgetFile "account")
+    mStatusParamText <- lookupGetParam "status"
+    case mStatusParamText of
+        Nothing -> redirect (AccountR accountIdParam, [("status", toParam POpen)])
+        Just statusParamText -> do
+            let statusParams = fromMaybe S.empty $ fromParam statusParamText
+            $(logDebug) $ "state params: " <> pack (show (statusParams :: Set StatusParam))
+            (account, rows) <- runDB $ do
+                accountEntity <- getBy404 (UniqueAccount accountIdParam)
+                let account = entityVal accountEntity
+                rows <- E.select $ E.from $ \(tw `E.InnerJoin` twc `E.InnerJoin` acc) -> do
+                    E.on $
+                        tw E.^. TweetId E.==. twc E.^. TweetCandidateTweetId
+                        E.&&. tw E.^. TweetAccountId E.==. acc E.^. AccountId
+                    E.where_ $
+                        acc E.^. AccountIdent E.==. E.val accountIdParam
+                        E.&&. E.notExists (E.from $ \twca ->
+                                               E.where_ $
+                                                   twca E.^. TweetCandidateTweetId E.==. tw E.^. TweetId
+                                                   E.&&. twca E.^. TweetCandidateCreated E.>. twc E.^. TweetCandidateCreated
+                                          )
+                    return (tw, twc)
+                return (account, rows)
+            defaultLayout $ do
+                headerWidget $ Just user
+                $(widgetFile "account")
 
 data StatusParam = POpen | PTweeted | PClosed
     deriving (Eq, Show, Ord)
@@ -43,7 +45,3 @@ instance Param StatusParam where
     fromParam "tweeted" = Just PTweeted
     fromParam "closed"  = Just PClosed
     fromParam _         = Nothing
-
-instance (Param a, Ord a) => Param (Set a) where
-    toParam = mconcat . intersperse "|" . map toParam . S.toList
-    fromParam = Just . S.fromList . mapMaybe fromParam . splitOn "|"
--- app/Model.hs
+++ app/Model.hs
@@ -5,6 +5,8 @@ module Model where
 import ClassyPrelude.Yesod
 import Database.Persist.Quasi
 import Text.Blaze
+import Data.Text (splitOn)
+import qualified Data.Set as S
 
 import Model.TweetStatus
 import Model.Permission
@@ -25,3 +27,7 @@ instance ToMarkup TweetId where
 class Param a where
     toParam :: a -> Text
     fromParam :: Text -> Maybe a
+
+instance (Param a, Ord a) => Param (Set a) where
+    toParam = mconcat . intersperse "|" . map toParam . S.toList
+    fromParam = Just . S.fromList . mapMaybe fromParam . splitOn "|"
