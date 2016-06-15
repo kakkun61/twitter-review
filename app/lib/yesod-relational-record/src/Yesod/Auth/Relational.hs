718497fb8d659ed0fb3675c2a2e600c311262653
Kazuki Okamoto <kazuki.okamoto@kakkun61.com>
2016-06-16 08:13:19 +0900

    Yesod.Auth.Relational 途中

--- app/lib/yesod-relational-record/src/Yesod/Auth/Relational.hs
+++ app/lib/yesod-relational-record/src/Yesod/Auth/Relational.hs
@@ -1,13 +1,22 @@
+{-# LANGUAGE TypeFamilies #-}
+
 module Yesod.Auth.Relational where
 
--- | Retrieves user credentials from the session, if user is authenticated.
---
--- This function does /not/ confirm that the credentials are valid, see
--- 'maybeAuthIdRaw' for more information.
---
--- Since 1.1.2
+import Control.Monad
+import Control.Monad.Trans.Maybe
+import Data.Typeable (Typeable)
+
+import Yesod.Core
+import Yesod.Auth (YesodAuth, AuthId, maybeAuthId, credsKey)
+import Yesod.Relational
+
+class (YesodAuth master, YesodRelational master) => YesodAuthRelational master where
+    type AuthModel master :: *
+
+    getAuthModel :: AuthId master -> HandlerT master IO (Maybe (AuthModel master))
+
 defaultMaybeAuthId
-    :: (YesodAuthPersist master, Typeable (AuthModel master))
+    :: (YesodAuthRelational master, Typeable (AuthModel master))
     => HandlerT master IO (Maybe (AuthId master))
 defaultMaybeAuthId = runMaybeT $ do
     s   <- MaybeT $ lookupSession credsKey
@@ -16,7 +25,7 @@ defaultMaybeAuthId = runMaybeT $ do
     return aid
 
 cachedAuth
-    :: (YesodAuthPersist master, Typeable (AuthModel master))
+    :: (YesodAuthRelational master, Typeable (AuthModel master))
     => AuthId master -> HandlerT master IO (Maybe (AuthModel master))
 cachedAuth
     = fmap unCachedMaybeAuth
@@ -27,64 +36,38 @@ cachedAuth
 newtype CachedMaybeAuth val = CachedMaybeAuth { unCachedMaybeAuth :: Maybe val }
     deriving Typeable
 
--- | Similar to 'maybeAuthId', but additionally look up the value associated
--- with the user\'s database identifier to get the value in the database. This
--- assumes that you are using a Persistent database.
---
--- Since 1.1.0
-maybeAuth :: ( YesodAuthPersist master
-             , val ~ AuthModel master
-             , Key val ~ AuthId master
-             , PersistEntity val
+maybeAuth :: ( YesodAuthRelational master
              , Typeable val
-             ) => HandlerT master IO (Maybe (Entity val))
+             ) => HandlerT master IO (Maybe (AuthModel master))
 maybeAuth = runMaybeT $ do
     (aid, ae) <- MaybeT maybeAuthPair
-    return $ Entity aid ae
+    return ae
 
--- | Similar to 'maybeAuth', but doesn’t assume that you are using a
--- Persistent database.
---
--- Since 1.4.0
-maybeAuthPair :: (YesodAuthPersist master, Typeable (AuthModel master))
+maybeAuthPair :: (YesodAuthRelational master, Typeable (AuthModel master))
               => HandlerT master IO (Maybe (AuthId master, AuthModel master))
 maybeAuthPair = runMaybeT $ do
     aid <- MaybeT maybeAuthId
     ae  <- MaybeT $ cachedAuth aid
     return (aid, ae)
 
--- | Class which states that the given site is an instance of @YesodAuth@
--- and that its @AuthId@ is a lookup key for the full user information in
--- a @YesodPersist@ database.
---
--- The default implementation of @getAuthModel@ assumes that the @AuthId@
--- for the @YesodAuth@ superclass is in fact a persistent @Key@ for the
--- given value.  This is the common case in Yesod, and means that you can
--- easily look up the full information on a given user.
---
--- Since 1.4.0
-class (YesodAuth master, YesodRelational master) => YesodAuthRelational master where
-    type AuthModel master :: *
-
-    getAuthModel :: AuthId master -> HandlerT master IO (Maybe (AuthModel master))
-
--- | Similar to 'maybeAuth', but redirects to a login page if user is not
--- authenticated or responds with error 401 if this is an API client (expecting JSON).
-requireAuth :: ( YesodAuthPersist master
-               , val ~ AuthModel master
-               , Key val ~ AuthId master
-               , PersistEntity val
+requireAuth :: ( YesodAuthRelational master
                , Typeable val
-               ) => HandlerT master IO (Entity val)
+               ) => HandlerT master IO (AuthModel master)
 requireAuth = maybeAuth >>= maybe handleAuthLack return
+
+requireAuthPair :: (YesodAuthRelational master, Typeable (AuthModel master))
+                => HandlerT master IO (AuthId master, AuthModel master)
+requireAuthPair = maybeAuthPair >>= maybe handleAuthLack return
 
 handleAuthLack :: Yesod master => HandlerT master IO a
 handleAuthLack = do
     aj <- acceptsJson
     if aj then notAuthenticated else redirectLogin
 
--- | Similar to 'requireAuth', but not tied to Persistent's 'Entity' type.
--- Instead, the 'AuthId' and 'AuthModel' are returned in a tuple.
-requireAuthPair :: (YesodAuthPersist master, Typeable (AuthModel master))
-                => HandlerT master IO (AuthId master, AuthModel master)
-requireAuthPair = maybeAuthPair >>= maybe handleAuthLack return
+redirectLogin :: Yesod master => HandlerT master IO a
+redirectLogin = do
+    y <- getYesod
+    setUltDestCurrent
+    case authRoute y of
+        Just z -> redirect z
+        Nothing -> permissionDenied "Please configure authRoute"
