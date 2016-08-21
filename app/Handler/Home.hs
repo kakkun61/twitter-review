efd21eff3a99279445dc910171c6e6a37ef5ce29
Kazuki Okamoto <kazuki.okamoto@kakkun61.com>
2016-08-22 03:52:24 +0900

    logined home logic with bug about mysql_client

--- app/Handler/Home.hs
+++ app/Handler/Home.hs
@@ -1,22 +1,27 @@
 module Handler.Home where
 
-import Import hiding (Set)
+import Import hiding (Set, on)
+import Database.Relational.Query
+import qualified Model.Table.User as User
+import qualified Model.Table.Account as Account
+import qualified Model.Table.UserAccountRelation as UAR
 
 getHomeR :: Handler Html
 getHomeR = do
     mUser <- maybeAuth
     $(logDebug) $ pack $ show mUser
     case mUser of
-        Just user -> error "under construction"
---             runDB $ do
---                 accounts <- ((entityVal <$>) <$>) $ E.select $ E.from $ \(acc `E.InnerJoin` rel) -> do
---                     E.on $ acc E.^. AccountId E.==. rel E.^. UserAccountRelationAccountId
---                     E.where_ $ rel E.^. UserAccountRelationUserId E.==. E.val (entityKey user)
---                     return acc
---                 lift $ defaultLayout $ do
---                     let signinWithGoogle = $(widgetFile "signin-with-google")
---                     headerWidget $ Just $ entityVal user
---                     $(widgetFile "home")
+        Just user -> do
+            accounts <- runRelational $ flip runQuery () $ relationalQuery $ relation $ do
+                a <- query Account.account
+                r <- query UAR.userAccountRelation
+                on $ a ! Account.id' .=. r ! UAR.accountId'
+                wheres $ r ! UAR.userId' .=. value (User.id user)
+                return a
+            $(logDebug) $ pack $ show accounts
+            defaultLayout $ do
+                headerWidget mUser
+                homeWidget mUser
         Nothing ->
             defaultLayout $ do
                 let accounts = []
