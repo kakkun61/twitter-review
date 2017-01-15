5705f27732a9cf50b184441f9e158f0e15066250
Kazuki Okamoto <kazuki.okamoto@kakkun61.com>
2017-01-16 05:41:07 +0900

    change tables

--- mysql/create-tables.sql
+++ mysql/create-tables.sql
@@ -4,27 +4,27 @@
 use `twitter-review`;
 
 CREATE TABLE `user` (
-  `id`           bigint(20)                        NOT NULL AUTO_INCREMENT,
-  `email`        varchar(254) CHARACTER SET latin1 NOT NULL,
-  `display_name` text,
-  `token`        text                              NOT NULL,
+  `id`           BIGINT UNSIGNED                   NOT NULL AUTO_INCREMENT,
+  `email`        VARCHAR(254) CHARACTER SET latin1 NOT NULL,
+  `display_name` TEXT,
+  `token`        TEXT                              NOT NULL,
   PRIMARY KEY (`id`),
   UNIQUE KEY `unique_user` (`email`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_bin;
 
 CREATE TABLE `account` (
-  `id`           bigint(20) NOT NULL,
-  `screen_name`  text       NOT NULL,
-  `token`        text       NOT NULL,
-  `token_secret` text       NOT NULL,
+  `id`           BIGINT UNSIGNED NOT NULL,
+  `screen_name`  TEXT            NOT NULL,
+  `token`        TEXT            NOT NULL,
+  `token_secret` TEXT            NOT NULL,
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_bin;
 
 CREATE TABLE `user_account_relation` (
-  `id`         bigint(20) NOT NULL AUTO_INCREMENT,
-  `user_id`    bigint(20) NOT NULL,
-  `account_id` bigint(20) NOT NULL,
-  `permission` int(11)    NOT NULL,
+  `id`         BIGINT UNSIGNED  NOT NULL AUTO_INCREMENT,
+  `user_id`    BIGINT UNSIGNED  NOT NULL,
+  `account_id` BIGINT UNSIGNED  NOT NULL,
+  `permission` TINYINT UNSIGNED NOT NULL,
   PRIMARY KEY (`id`),
   UNIQUE KEY `unique_user_account_relation` (`user_id`,`account_id`),
   KEY `user_account_relation_user_id_fkey` (`user_id`),
@@ -34,11 +34,11 @@ CREATE TABLE `user_account_relation` (
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_bin;
 
 CREATE TABLE `tweet` (
-  `id`         bigint(20) NOT NULL AUTO_INCREMENT,
-  `account_id` bigint(20) NOT NULL,
-  `user_id`    bigint(20) NOT NULL,
-  `status`     text       NOT NULL,
-  `created`    datetime   NOT NULL,
+  `id`         BIGINT UNSIGNED   NOT NULL AUTO_INCREMENT,
+  `account_id` BIGINT UNSIGNED   NOT NULL,
+  `user_id`    BIGINT UNSIGNED   NOT NULL,
+  `status`     TINYINT UNSIGNED  NOT NULL,
+  `created`    DATETIME          NOT NULL,
   PRIMARY KEY (`id`),
   KEY `tweet_account_id_fkey` (`account_id`),
   KEY `tweet_user_id_fkey` (`user_id`),
@@ -46,12 +46,19 @@ CREATE TABLE `tweet` (
   CONSTRAINT `tweet_user_id_fkey` FOREIGN KEY (`user_id`) REFERENCES `user` (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_bin;
 
+CREATE TABLE `tweet_uri` (
+  `tweet_id` BIGINT UNSIGNED NOT NULL,
+  `uri`      TEXT            NOT NULL,
+  PRIMARY KEY (`tweet_id`),
+  CONSTRAINT `tweet_uri_tweet_id_fkey` FOREIGN KEY (`tweet_id`) REFERENCES `tweet` (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_bin;
+
 CREATE TABLE `tweet_candidate` (
-  `id`       bigint(20) NOT NULL AUTO_INCREMENT,
-  `tweet_id` bigint(20) NOT NULL,
-  `text`     text       NOT NULL,
-  `user_id`  bigint(20) NOT NULL,
-  `created`  datetime   NOT NULL,
+  `id`       BIGINT UNSIGNED   NOT NULL AUTO_INCREMENT,
+  `tweet_id` BIGINT UNSIGNED   NOT NULL,
+  `TEXT`     TEXT              NOT NULL,
+  `user_id`  BIGINT UNSIGNED   NOT NULL,
+  `created`  DATETIME          NOT NULL,
   PRIMARY KEY (`id`),
   KEY `tweet_candidate_tweet_id_fkey` (`tweet_id`),
   CONSTRAINT `tweet_candidate_tweet_id_fkey` FOREIGN KEY (`tweet_id`) REFERENCES `tweet` (`id`),
@@ -59,11 +66,11 @@ CREATE TABLE `tweet_candidate` (
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_bin;
 
 CREATE TABLE `comment` (
-  `id`       bigint(20) NOT NULL AUTO_INCREMENT,
-  `tweet_id` bigint(20) NOT NULL,
-  `text`     text       NOT NULL,
-  `user`     bigint(20) NOT NULL,
-  `created`  datetime   NOT NULL,
+  `id`       BIGINT UNSIGNED   NOT NULL AUTO_INCREMENT,
+  `tweet_id` BIGINT UNSIGNED   NOT NULL,
+  `TEXT`     TEXT              NOT NULL,
+  `user`     BIGINT UNSIGNED   NOT NULL,
+  `created`  DATETIME          NOT NULL,
   PRIMARY KEY (`id`),
   KEY `comment_tweet_id_fkey` (`tweet_id`),
   CONSTRAINT `comment_user_fkey` FOREIGN KEY (`user`) REFERENCES `user` (`id`)
