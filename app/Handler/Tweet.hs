22847d56159a14888632dc6e5bf3384de9092475
Kazuki Okamoto <kazuki.okamoto@kakkun61.com>
2017-01-31 19:41:23 +0900

    use selectFoo

--- app/Handler/Tweet.hs
+++ app/Handler/Tweet.hs
@@ -11,22 +11,13 @@ import qualified Model.Table.User as User
 getTweetR :: AccountIdParam -> TweetIdParam -> Handler Html
 getTweetR accountIdParam tweetIdParam = runRelational $ do
     user <- lift requireAuth
-    accounts <- flip runQuery () $ relationalQuery $ relation $ do
-                    a <- query Account.account
-                    wheres $ a ! Account.id' .=. value accountIdParam
-                    return a
+    accounts <- runQuery Account.selectAccount accountIdParam
     case accounts of
         [account@(Account _ _ _ _)] -> do
-            tweets <- flip runQuery () $ relationalQuery $ relation $ do
-                          t <- query Tweet.tweet
-                          wheres $ t ! Tweet.id' .=. value tweetIdParam
-                          return t
+            tweets <- runQuery Tweet.selectTweet tweetIdParam
             case tweets of
                 [tweet@(Tweet _ _ tweetUserId _ _)] -> do
-                    tweetUsers <- flip runQuery () $ relationalQuery $ relation $ do
-                                     u <- query User.user
-                                     wheres $ u ! User.id' .=. value tweetUserId
-                                     return u
+                    tweetUsers <- runQuery User.selectUser tweetUserId
                     case tweetUsers of
                         [tweetUser@(User _ _ _ _)] -> do
                             lift $ defaultLayout $ do
